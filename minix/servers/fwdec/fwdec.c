479d21c32a36358d37dce401115ca08a784134b9
Daniel <ddah@kth.se>
2019-12-13 14:54:31 +0100

    Implemented syscall interface for FW

--- minix/servers/fwdec/fwdec.c
+++ minix/servers/fwdec/fwdec.c
@@ -19,6 +19,10 @@
 /* Declare local functions. */
 static fw_rule_t *find_matching_rule(fw_rule_t *rules, uint32_t ip_addr);
 static void log(char* log_message);
+int find_rule(fw_rule_t,fw_rule_t);
+fw_rule_t* last_rule(fw_rule_t* currRule);
+fw_rule_t* find_rule_before(fw_rule_t* currRule,fw_rule_t* ruleTofind);
+char compare_rules(fw_rule_t* rule1,fw_rule_t* rule2);
 
 static fw_rule_t default_incoming_rule = {
   .from_ip = IP_ANY,
@@ -83,7 +87,7 @@ int check_outgoing_ip4(uint32_t dest_ip) {
   }
 
   log("Packet accepted\n");
-  return LWIP_KEEP_PACKET; 
+  return LWIP_KEEP_PACKET;
 }
 
 static fw_rule_t *find_matching_rule(fw_rule_t *rules, uint32_t ip_addr)
@@ -111,7 +115,7 @@ static fw_rule_t *find_matching_rule(fw_rule_t *rules, uint32_t ip_addr)
       chosen_rule = curr_rule;
       chosen_flags = curr_flags;
     }
-    
+
     curr_rule = curr_rule->next;
   }
 
@@ -134,4 +138,69 @@ static void log(char* log_message)
     printf("Warning: fwdec failed to write to log file");
   }
   close(fd);
+}
+
+int add_rule(uint32_t src_ip, uint32_t dest_ip, char* p_name, uint8_t action){
+  fw_rule_t* finalRule = last_rule(&default_incoming_rule);
+  if(finalRule){
+    return -1;
+  }
+  fw_rule_t* tmp = malloc(sizeof(fw_rule_t));
+  tmp -> from_ip = src_ip;
+  tmp -> to_ip = dest_ip;
+  tmp -> p_name = p_name;
+  tmp -> action = action;
+  tmp -> next = NULL;
+  finalRule -> next = tmp;
+  return 1;
+}
+
+int remove_rule(uint32_t src_ip, uint32_t dest_ip, char* p_name, uint8_t action){
+  fw_rule_t* tmp = malloc(sizeof(fw_rule_t));
+  tmp -> from_ip = src_ip;
+  tmp -> to_ip = dest_ip;
+  tmp -> p_name = p_name;
+  tmp -> action = action;
+  tmp -> next = NULL;
+  fw_rule_t* tmp2;
+  tmp2 = find_rule_before(tmp,&default_incoming_rule);
+  if(tmp2){
+    free(tmp2 -> next);
+    free(tmp -> next);
+    tmp2 -> next = (tmp2 -> next) -> next;
+  } else {
+    free(tmp);
+    return -1;
+  }
+  free(tmp);
+  return 1;
+}
+
+fw_rule_t* find_rule_before(fw_rule_t* currRule,fw_rule_t* ruleTofind){
+  if(compare_rules(currRule,ruleTofind)){
+    default_incoming_rule = *(currRule -> next);
+  }
+  while(currRule -> next!=NULL){
+    if(compare_rules(currRule -> next,ruleTofind)){
+      return currRule;
+    }
+    currRule=currRule -> next;
+  }
+  return NULL;
+}
+
+char compare_rules(fw_rule_t* rule1,fw_rule_t* rule2){
+  if(rule1 -> from_ip==rule2 -> from_ip)
+    if(rule1 -> to_ip==rule2 -> to_ip)
+      if(strcmp(rule1 -> p_name,rule2 -> p_name))
+        if(rule1 -> action==rule2 -> action)
+          return 1;
+  return 0;
+}
+
+fw_rule_t* last_rule(fw_rule_t* currRule){
+    while(currRule -> next){
+      currRule=currRule -> next;
+    }
+    return currRule;
 }
