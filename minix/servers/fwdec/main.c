be4258656aa11908e69c4ddb656a4c7496fddb67
André Brogärd <brogard@kth.se>
2021-12-02 13:30:44 +0100

    Fixes print bug, adds ttys modification

--- minix/servers/fwdec/main.c
+++ minix/servers/fwdec/main.c
@@ -39,6 +39,8 @@ int main(int argc, char **argv)
   message m;
   int result;
 
+  uid_t effuid = 10;
+
   /* SEF local startup. */
   env_setargs(argc, argv);
   sef_local_startup();
@@ -69,11 +71,10 @@ int main(int argc, char **argv)
       result = OK;
       break;
     default:
-      uid_t effuid = 10;
       if(m.m_fwdec_ip4.user_endp) {
         getepname(m.m_fwdec_ip4.user_endp, proc_name, 16); // cannot fail
         getepeffuid(m.m_fwdec_ip4.user_endp, &effuid); // TODO: NOW IF THIS FAILS, effuid still root => 0
-        printf(effuid)
+        printf("uid: %d \n", effuid);
       }
       src_ip = m.m_fwdec_ip4.src_ip;
       dest_ip = m.m_fwdec_ip4.dest_ip;
