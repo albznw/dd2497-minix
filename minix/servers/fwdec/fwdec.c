22b81309deb658aab92c82a1cf74d1e3a494a62a
André Brogärd <brogard@kth.se>
2021-12-02 16:47:56 +0100

    Define chains and add add_chain_rule function
    Co-authored-by: Henrik Kultala <kultala.henrik@gmail.com>
    Co-authored-by: Albin Winkelmann <albin-w@hotmail.com>
    Co-authored-by: Tommie Andersson <tommiea_96@hotmail.com>

--- minix/servers/fwdec/fwdec.c
+++ minix/servers/fwdec/fwdec.c
@@ -23,8 +23,10 @@
 
 /* Declare local functions. */
 #ifdef FWDEC_DEBUG
-static void debug_log_packet(const int type, const int result, const uint32_t src_ip, const uint32_t dest_ip,
-                              const uint16_t src_port, const uint16_t dest_port, const char* p_name);
+static void debug_log_packet(const int type, const int result,
+                             const uint32_t src_ip, const uint32_t dest_ip,
+                             const uint16_t src_port, const uint16_t dest_port,
+                             const char* p_name);
 #endif
 
 static void log(char* log_message);
@@ -32,57 +34,56 @@ static void log(char* log_message);
 static fw_rule* out_rules = NULL;
 static fw_rule* in_rules = NULL;
 
-static inline uint32_t ip4_from_parts(uint8_t p1, uint8_t p2, uint8_t p3, uint8_t p4)
-{
+static fw_chain* chain = NULL;
+
+static inline uint32_t ip4_from_parts(uint8_t p1, uint8_t p2, uint8_t p3,
+                                      uint8_t p4) {
   uint32_t result = p4 << 24 | p3 << 16 | p2 << 8 | p1;
   return result;
 }
 
 /* Global variables - Configurables */
-const char *LOGFILE = "/var/log/fwdec"; //Where the log file should be placed
+const char* LOGFILE = "/var/log/fwdec";  // Where the log file should be placed
 
 /*===========================================================================*
- *		            sef_cb_init_fresh                                        *
+ *		            sef_cb_init_fresh *
  *===========================================================================*/
-int sef_cb_init_fresh(int UNUSED(type), sef_init_info_t *info)
-{
+int sef_cb_init_fresh(int UNUSED(type), sef_init_info_t* info) {
+  chain = (fw_chain*)malloc(sizeof(fw_chain));
+  chain->head_entry = NULL;
   // Push default rules
-  push_rule(&out_rules, IP_ANY, IP_ANY, FW_IP, 0, MIN_PRIORITY, FW_RULE_ACCEPT, NULL);
-  push_rule(&in_rules, IP_ANY, IP_ANY, FW_IP, 0, MIN_PRIORITY, FW_RULE_ACCEPT, NULL);
-  
+  // push_rule(&out_rules, IP_ANY, IP_ANY, FW_IP, 0, MIN_PRIORITY,
+  // FW_RULE_ACCEPT, NULL); push_rule(&in_rules, IP_ANY, IP_ANY, FW_IP, 0,
+  // MIN_PRIORITY, FW_RULE_ACCEPT, NULL);
+
   // Push custom hard-coded rules
   uint32_t kth_ip = ip4_from_parts(130, 237, 28, 40);
   uint32_t google_dns = ip4_from_parts(8, 8, 8, 8);
   uint32_t local_host = ip4_from_parts(127, 0, 0, 1);
-  push_rule(&in_rules, google_dns, google_dns, FW_TCP, 0, MAX_PRIORITY, FW_RULE_REJECT, "dig");
-  push_rule(&in_rules, local_host, local_host, FW_TCP, 80, MAX_PRIORITY, FW_RULE_REJECT, "telnet");
+  push_rule(&in_rules, google_dns, google_dns, FW_TCP, 0, MAX_PRIORITY,
+            FW_RULE_REJECT, "dig");
+  push_rule(&in_rules, local_host, local_host, FW_TCP, 80, MAX_PRIORITY,
+            FW_RULE_REJECT, "telnet");
 
-  push_rule(&out_rules, kth_ip, kth_ip, FW_IP, 0, MAX_PRIORITY, FW_RULE_REJECT, NULL);
-  push_rule(&out_rules, google_dns, google_dns, FW_UDP, 53, MAX_PRIORITY, FW_RULE_ACCEPT, "dig");
-  push_rule(&out_rules, IP_ANY, IP_ANY, FW_UDP, 0, MED_PRIORITY, FW_RULE_REJECT, "dig");
+  push_rule(&out_rules, kth_ip, kth_ip, FW_IP, 0, MAX_PRIORITY, FW_RULE_REJECT,
+            NULL);
+  push_rule(&out_rules, google_dns, google_dns, FW_UDP, 53, MAX_PRIORITY,
+            FW_RULE_ACCEPT, "dig");
+  push_rule(&out_rules, IP_ANY, IP_ANY, FW_UDP, 0, MED_PRIORITY, FW_RULE_REJECT,
+            "dig");
 
   printf("Firewall decision server started\n");
-  return(OK);
+  return (OK);
 }
 
 /*===========================================================================*
- *				do_publish				                                     *
+ *				do_publish *
  *===========================================================================*/
 
-int check_incoming(const uint8_t type, const uint32_t src_ip, const uint16_t port, const char *p_name) {
-  fw_rule *matched_rule = find_matching_rule(&in_rules, type, src_ip, port, p_name);
-
-  if (matched_rule->action == FW_RULE_REJECT) {
-    log("Packet dropped\n");
-    return LWIP_DROP_PACKET;
-  }
-
-  log("Packet accepted\n");
-  return LWIP_KEEP_PACKET; 
-}
-
-int check_outgoing(const uint8_t type, const uint32_t dest_ip, const uint16_t port, const char *p_name) {
-  fw_rule *matched_rule = find_matching_rule(&out_rules, type, dest_ip, port, p_name);
+int check_incoming(const uint8_t type, const uint32_t src_ip,
+                   const uint16_t port, const char* p_name) {
+  fw_rule* matched_rule =
+      find_matching_rule(&in_rules, type, src_ip, port, p_name);
 
   if (matched_rule->action == FW_RULE_REJECT) {
     log("Packet dropped\n");
@@ -93,7 +94,22 @@ int check_outgoing(const uint8_t type, const uint32_t dest_ip, const uint16_t po
   return LWIP_KEEP_PACKET;
 }
 
-int check_incoming_tcp(const uint32_t src_ip, const uint16_t port, const char *p_name, uint64_t flags) {
+int check_outgoing(const uint8_t type, const uint32_t dest_ip,
+                   const uint16_t port, const char* p_name) {
+  fw_rule* matched_rule =
+      find_matching_rule(&out_rules, type, dest_ip, port, p_name);
+
+  if (matched_rule->action == FW_RULE_REJECT) {
+    log("Packet dropped\n");
+    return LWIP_DROP_PACKET;
+  }
+
+  log("Packet accepted\n");
+  return LWIP_KEEP_PACKET;
+}
+
+int check_incoming_tcp(const uint32_t src_ip, const uint16_t port,
+                       const char* p_name, uint64_t flags) {
   // Let the TCP server do TCP related analysis such as SYN-FLOOD prevention
   if (fwtcp_check_packet(src_ip, flags) != LWIP_KEEP_PACKET) {
     log("Packet dropped\n");
@@ -103,15 +119,17 @@ int check_incoming_tcp(const uint32_t src_ip, const uint16_t port, const char *p
 }
 
 int add_rule(uint8_t direction, uint8_t type, uint8_t priority, uint8_t action,
-				uint32_t ip_start, uint32_t ip_end, uint16_t port, char* p_name) {
+             uint32_t ip_start, uint32_t ip_end, uint16_t port, char* p_name) {
   printf("fwdec: adding rule\n");
   switch (direction) {
-  case 0:
-    push_rule(&in_rules, ip_start, ip_end, type, port, priority, action, *p_name != '\0' ? p_name : NULL);
-    break; 
-  default:
-    push_rule(&out_rules, ip_start, ip_end, type, port, priority, action, *p_name != '\0' ? p_name : NULL);
-    break;
+    case 0:
+      push_rule(&in_rules, ip_start, ip_end, type, port, priority, action,
+                *p_name != '\0' ? p_name : NULL);
+      break;
+    default:
+      push_rule(&out_rules, ip_start, ip_end, type, port, priority, action,
+                *p_name != '\0' ? p_name : NULL);
+      break;
   }
   return 0;
 }
@@ -122,22 +140,26 @@ void list_rules(void) {
   return;
 }
 
-int delete_rule(uint8_t direction, uint8_t type, uint8_t priority, uint8_t action,
-					uint32_t ip_start, uint32_t ip_end, uint16_t port, char* p_name) {
+int delete_rule(uint8_t direction, uint8_t type, uint8_t priority,
+                uint8_t action, uint32_t ip_start, uint32_t ip_end,
+                uint16_t port, char* p_name) {
   printf("fwdec: removing rule\n");
   switch (direction) {
-  case 0:
-    remove_rule(&in_rules, ip_start, ip_end, type, port, priority, action, *p_name != '\0' ? p_name : NULL);
-    break; 
-  default:
-    remove_rule(&out_rules, ip_start, ip_end, type, port, priority, action, *p_name != '\0' ? p_name : NULL);
-    break;
+    case 0:
+      remove_rule(&in_rules, ip_start, ip_end, type, port, priority, action,
+                  *p_name != '\0' ? p_name : NULL);
+      break;
+    default:
+      remove_rule(&out_rules, ip_start, ip_end, type, port, priority, action,
+                  *p_name != '\0' ? p_name : NULL);
+      break;
   }
   return 0;
 }
 
 int check_packet(const int type, const uint32_t src_ip, const uint32_t dest_ip,
-                  const uint16_t src_port, const uint16_t dest_port, const char* p_name, const uint64_t flags) {
+                 const uint16_t src_port, const uint16_t dest_port,
+                 const char* p_name, const uint64_t flags) {
   int result;
 
   switch (type) {
@@ -183,44 +205,49 @@ int check_packet(const int type, const uint32_t src_ip, const uint32_t dest_ip,
       result = EINVAL;
   }
 
-  #ifdef FWDEC_DEBUG
+#ifdef FWDEC_DEBUG
   debug_log_packet(type, result, src_ip, dest_ip, src_port, dest_port, p_name);
-  #endif
+#endif
 
   return result;
 }
 
-static void log(char* log_message)
-{
+static void log(char* log_message) {
   // strncat(log_message, "\n", 1);
-  int fd = open(LOGFILE, O_WRONLY|O_CREAT|O_APPEND);
+  int fd = open(LOGFILE, O_WRONLY | O_CREAT | O_APPEND);
 
-  if (fd == -1){
+  if (fd == -1) {
     printf("Warning: fwdec failed to open log file %s\n", LOGFILE);
     return;
   }
 
   int length = strlen(log_message);
   int written = write(fd, log_message, length);
-  if (written != length){
+  if (written != length) {
     printf("Warning: fwdec failed to write to log file");
   }
   close(fd);
 }
 
 #ifdef FWDEC_DEBUG
-static void debug_log_packet(const int type, const int result, const uint32_t src_ip, const uint32_t dest_ip,
-                              const uint16_t src_port, const uint16_t dest_port, const char* p_name) {
-
-  unsigned char src_bytes[4] = {src_ip & 0xFF, (src_ip >> 8) & 0xFF, (src_ip >> 16) & 0xFF, (src_ip >> 24) & 0xFF};
-  unsigned char dest_bytes[4] = {dest_ip & 0xFF, (dest_ip >> 8) & 0xFF, (dest_ip >> 16) & 0xFF, (dest_ip >> 24) & 0xFF};
+static void debug_log_packet(const int type, const int result,
+                             const uint32_t src_ip, const uint32_t dest_ip,
+                             const uint16_t src_port, const uint16_t dest_port,
+                             const char* p_name) {
+  unsigned char src_bytes[4] = {src_ip & 0xFF, (src_ip >> 8) & 0xFF,
+                                (src_ip >> 16) & 0xFF, (src_ip >> 24) & 0xFF};
+  unsigned char dest_bytes[4] = {dest_ip & 0xFF, (dest_ip >> 8) & 0xFF,
+                                 (dest_ip >> 16) & 0xFF,
+                                 (dest_ip >> 24) & 0xFF};
   char src_ip_str[64];
   char dest_ip_str[64];
-  snprintf(src_ip_str, 64, "%d.%d.%d.%d", src_bytes[0], src_bytes[1], src_bytes[2], src_bytes[3]);
-  snprintf(dest_ip_str, 64, "%d.%d.%d.%d", dest_bytes[0], dest_bytes[1], dest_bytes[2], dest_bytes[3]);
+  snprintf(src_ip_str, 64, "%d.%d.%d.%d", src_bytes[0], src_bytes[1],
+           src_bytes[2], src_bytes[3]);
+  snprintf(dest_ip_str, 64, "%d.%d.%d.%d", dest_bytes[0], dest_bytes[1],
+           dest_bytes[2], dest_bytes[3]);
 
   char result_str[8];
-  if(result == LWIP_DROP_PACKET){
+  if (result == LWIP_DROP_PACKET) {
     strcpy(result_str, "BLOCKED");
   } else {
     strcpy(result_str, "ALLOWED");
@@ -228,22 +255,28 @@ static void debug_log_packet(const int type, const int result, const uint32_t sr
 
   switch (type) {
     case FWDEC_QUERY_TCP_INC:
-      printf("TCP IN %s:%d <- %s:%d (%s) %s\n", dest_ip_str, dest_port, src_ip_str, src_port, p_name, result_str);
+      printf("TCP IN %s:%d <- %s:%d (%s) %s\n", dest_ip_str, dest_port,
+             src_ip_str, src_port, p_name, result_str);
       break;
     case FWDEC_QUERY_TCP_OUT:
-      printf("TCP OUT %s:%d -> %s:%d (%s) %s\n", src_ip_str, src_port, dest_ip_str, dest_port, p_name, result_str);
+      printf("TCP OUT %s:%d -> %s:%d (%s) %s\n", src_ip_str, src_port,
+             dest_ip_str, dest_port, p_name, result_str);
       break;
     case FWDEC_QUERY_UDP_INC:
-      printf("UDP IN %s:%d <- %s:%d (%s) %s\n", dest_ip_str, dest_port, src_ip_str, src_port, p_name, result_str);
+      printf("UDP IN %s:%d <- %s:%d (%s) %s\n", dest_ip_str, dest_port,
+             src_ip_str, src_port, p_name, result_str);
       break;
     case FWDEC_QUERY_UDP_OUT:
-      printf("UDP OUT %s:%d -> %s:%d (%s) %s\n", src_ip_str, src_port, dest_ip_str, dest_port, p_name, result_str);
+      printf("UDP OUT %s:%d -> %s:%d (%s) %s\n", src_ip_str, src_port,
+             dest_ip_str, dest_port, p_name, result_str);
       break;
     case FWDEC_QUERY_RAW_INC:
-      printf("RAW IN %s <- %s (%s) %s\n", dest_ip_str, src_ip_str, p_name, result_str);
+      printf("RAW IN %s <- %s (%s) %s\n", dest_ip_str, src_ip_str, p_name,
+             result_str);
       break;
     case FWDEC_QUERY_RAW_OUT:
-      printf("RAW OUT %s -> %s (%s) %s\n", src_ip_str, dest_ip_str, p_name, result_str);
+      printf("RAW OUT %s -> %s (%s) %s\n", src_ip_str, dest_ip_str, p_name,
+             result_str);
       break;
     case FWDEC_QUERY_ICMP_INC:
       printf("ICMP IN %s <- %s %s\n", dest_ip_str, src_ip_str, result_str);
@@ -251,7 +284,7 @@ static void debug_log_packet(const int type, const int result, const uint32_t sr
     case FWDEC_QUERY_ICMP_OUT:
       printf("ICMP OUT %s -> %s %s\n", src_ip_str, dest_ip_str, result_str);
       break;
-    default: 
+    default:
       break;
   }
 }
--- minix/servers/fwdec/fwrule.h
+++ minix/servers/fwdec/fwrule.h
@@ -1,10 +1,10 @@
 #ifndef _FWDEC_RULE_H_
 #define _FWDEC_RULE_H_
 
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdint.h>
 
 #define MAX_PRIORITY 0xFF
 #define MED_PRIORITY 0xAA
@@ -12,168 +12,290 @@
 
 #define MAX_NAME_LEN 16
 
+#define NOUID (uid_t)0x10
+
 /**
  * Let fw_rules be a doubly linked list that exists on the HEAP.
  */
 typedef struct fw_rule {
-    uint8_t type;
-    uint8_t priority;
-    uint8_t action;
-    uint32_t ip_start;
-    uint32_t ip_end;
-    uint16_t port;
-    char p_name[MAX_NAME_LEN];
-    struct fw_rule* next;
-    struct fw_rule* prev;
+  uint8_t type;
+  uint8_t priority;
+  uint8_t action;
+  uint32_t ip_start;
+  uint32_t ip_end;
+  uint16_t port;
+  char p_name[MAX_NAME_LEN];
+  struct fw_rule *next;
+  struct fw_rule *prev;
 } fw_rule;
+
+#define IN_CHAIN 0
+#define OUT_CHAIN 1
+// Struct for firewall chains
+typedef struct fw_chain_rule {
+  uint8_t type;
+  uint8_t action;
+  uint8_t direction;  // IN_CHAIN or OUT_CHAIN
+  uid_t user;
+  uint32_t ip_start;
+  uint32_t ip_end;
+  uint16_t port;
+  char p_name[MAX_NAME_LEN];
+} fw_chain_rule;
+
+typedef struct fw_chain_entry {
+  struct fw_chain_rule *rule;
+  struct fw_chain_entry *prev;
+  struct fw_chain_entry *next;
+} fw_chain_entry;
+
+typedef struct fw_chain {
+  struct fw_chain_entry *head_entry;
+} fw_chain;
+
+/**
+ * Adds a new rule in the chain, if the wanted index is not found the
+ * rule will be placed in the end of the chain. A fast way to add rules
+ * to the end of the list is to add them at index -1.
+ *
+ * @param new_rule pointer to the new rule on the heap
+ * @param index wanted index
+ */
+void add_chain_rule(fw_chain *chain, fw_chain_rule *new_rule, int index) {
+  printf("Adding new rule %d %d %s", new_rule->type, new_rule->action,
+         new_rule->direction ? "OUT" : "IN");
+
+  fw_chain_entry *c_entry = chain->head_entry;
+  fw_chain_entry *new_entry = (fw_chain_entry *)malloc(sizeof(fw_chain_entry));
+
+  // Create a new entry from the added rule
+  new_entry->rule = new_rule;
+  new_entry->prev = NULL;
+  new_entry->next = NULL;
+
+  // No entries currently exists
+  if (chain->head_entry == NULL) {
+    // If we have no rules in the chain yet
+    new_entry->prev = NULL;
+    new_entry->next = NULL;
+    chain->head_entry = new_entry;
+    return;
+  }
+
+  int c_index = 0;
+  fw_chain_entry *last_entry = NULL;
+
+  while (c_entry) {
+    if (index == c_index) {
+      // Add new entry before c_entry
+
+      // entry before the new entry;
+      fw_chain_entry *tmp_prev = c_entry->prev;
+      // New references of c_entry
+      c_entry->prev = new_entry;
+      // New references of new entry
+      new_entry->next = c_entry;
+      new_entry->prev = tmp_prev;
+      // New reference of previous
+      if (tmp_prev != NULL) {
+        tmp_prev->next = new_entry;
+      }
+      return;
+    }
+    c_index++;
+    last_entry = c_entry;
+    c_entry = c_entry->next;
+  }
+
+  // Index not found, placing the new rule at the end
+  last_entry->next = new_entry;
+  new_entry->prev = last_entry;
+}
 
 /**
  * Find first matching rule with highest priority.
  */
-fw_rule *find_matching_rule(fw_rule** head_ref, const uint8_t type, const uint32_t ip_addr, const uint16_t port,
-                                const char *p_name) {
+fw_rule *find_matching_rule(fw_rule **head_ref, const uint8_t type,
+                            const uint32_t ip_addr, const uint16_t port,
+                            const char *p_name) {
+  uint8_t highest_prio = MIN_PRIORITY;
+  fw_rule *chosen_rule = NULL;
 
-    uint8_t highest_prio = MIN_PRIORITY;
-    fw_rule *chosen_rule = NULL;
+  fw_rule *curr_rule = (*head_ref);
 
-    fw_rule *curr_rule = (*head_ref);
-
-    while (curr_rule != NULL){
-        if (((curr_rule->ip_start == 0 && curr_rule->ip_end == 0)
-            || (curr_rule->ip_start <= ip_addr && ip_addr <= curr_rule->ip_end))
-            && (curr_rule->port == 0 || curr_rule->port == port) && (curr_rule->type == 0 || curr_rule->type == type)) {
-
-            // ip_addr is in this rules range
-            if(curr_rule->p_name[0] != '\0'){
-                // If function called with p_name and rule has a non-empty p_name, make sure they match
-                if(p_name != NULL && strncmp(curr_rule->p_name, p_name, MAX_NAME_LEN) == 0){
-                    if(curr_rule->priority >= highest_prio) {
-                        chosen_rule = curr_rule;
-                        highest_prio = curr_rule->priority;
-                    }
-                }
-            } else {
-                // Rule has no p_name
-                if(curr_rule->priority >= highest_prio) {
-                    chosen_rule = curr_rule;
-                    highest_prio = curr_rule->priority;
-                }
-            }
+  while (curr_rule != NULL) {
+    if (((curr_rule->ip_start == 0 && curr_rule->ip_end == 0) ||
+         (curr_rule->ip_start <= ip_addr && ip_addr <= curr_rule->ip_end)) &&
+        (curr_rule->port == 0 || curr_rule->port == port) &&
+        (curr_rule->type == 0 || curr_rule->type == type)) {
+      // ip_addr is in this rules range
+      if (curr_rule->p_name[0] != '\0') {
+        // If function called with p_name and rule has a non-empty p_name, make
+        // sure they match
+        if (p_name != NULL &&
+            strncmp(curr_rule->p_name, p_name, MAX_NAME_LEN) == 0) {
+          if (curr_rule->priority >= highest_prio) {
+            chosen_rule = curr_rule;
+            highest_prio = curr_rule->priority;
+          }
         }
-        curr_rule = curr_rule->next;
+      } else {
+        // Rule has no p_name
+        if (curr_rule->priority >= highest_prio) {
+          chosen_rule = curr_rule;
+          highest_prio = curr_rule->priority;
+        }
+      }
     }
+    curr_rule = curr_rule->next;
+  }
 
-    return chosen_rule;
+  return chosen_rule;
 }
 
-void get_ip_string(char* buf, uint32_t buf_len, uint32_t ip_addr){
-    unsigned char bytes[4] = {ip_addr & 0xFF, (ip_addr >> 8) & 0xFF, (ip_addr >> 16) & 0xFF, (ip_addr >> 24) & 0xFF};
-    snprintf(buf, buf_len, "%d.%d.%d.%d", bytes[0], bytes[1], bytes[2], bytes[3]);
+void get_ip_string(char *buf, uint32_t buf_len, uint32_t ip_addr) {
+  unsigned char bytes[4] = {ip_addr & 0xFF, (ip_addr >> 8) & 0xFF,
+                            (ip_addr >> 16) & 0xFF, (ip_addr >> 24) & 0xFF};
+  snprintf(buf, buf_len, "%d.%d.%d.%d", bytes[0], bytes[1], bytes[2], bytes[3]);
 }
 
 /**
  * Print the named chain of rules.
  */
-void print_rules(fw_rule** head_ref, const char* chain) {
-    printf("Chain %s\n", chain);
-    printf("%-5s%-8s%-9s%-16s%-16s%-6s%-16s\n", "type", "action", "priority", "start", "end", "port", "name");
-    fw_rule *curr_rule = (*head_ref);
-    while (curr_rule != NULL){
-        char action[7];
-        char type_str[5];
-        char start_ip_str[64];
-        char end_ip_str[64];
-        get_ip_string(start_ip_str, 64, curr_rule->ip_start);
-        get_ip_string(end_ip_str, 64, curr_rule->ip_end);
-        if(curr_rule->action == 1){
-            strncpy(action, "REJECT", 7);
-        } else {
-            strncpy(action, "ACCEPT", 7);
-        }
-        if(curr_rule->type == FW_TCP){
-            strncpy(type_str, "TCP", 5);
-        } else if(curr_rule->type == FW_UDP){
-            strncpy(type_str, "UDP", 5);
-        } else if(curr_rule->type == FW_ICMP){
-            strncpy(type_str, "ICMP", 5);
-        } else if(curr_rule->type == FW_RAW){
-            strncpy(type_str, "RAW", 5);
-        } else {
-            strncpy(type_str, "IP", 5);
-        }
-        printf("%-5s%-8s%-9d%-16s%-16s%-6d%-16s\n", type_str, action, curr_rule->priority, start_ip_str, end_ip_str,
-                curr_rule->port, curr_rule->p_name);
-        curr_rule = curr_rule->next;
+void print_rules(fw_rule **head_ref, const char *chain) {
+  printf("Chain %s\n", chain);
+  printf("%-5s%-8s%-9s%-16s%-16s%-6s%-16s\n", "type", "action", "priority",
+         "start", "end", "port", "name");
+  fw_rule *curr_rule = (*head_ref);
+  while (curr_rule != NULL) {
+    char action[7];
+    char type_str[5];
+    char start_ip_str[64];
+    char end_ip_str[64];
+    get_ip_string(start_ip_str, 64, curr_rule->ip_start);
+    get_ip_string(end_ip_str, 64, curr_rule->ip_end);
+    if (curr_rule->action == 1) {
+      strncpy(action, "REJECT", 7);
+    } else {
+      strncpy(action, "ACCEPT", 7);
     }
-    printf("\n");
-    return;
+    if (curr_rule->type == FW_TCP) {
+      strncpy(type_str, "TCP", 5);
+    } else if (curr_rule->type == FW_UDP) {
+      strncpy(type_str, "UDP", 5);
+    } else if (curr_rule->type == FW_ICMP) {
+      strncpy(type_str, "ICMP", 5);
+    } else if (curr_rule->type == FW_RAW) {
+      strncpy(type_str, "RAW", 5);
+    } else {
+      strncpy(type_str, "IP", 5);
+    }
+    printf("%-5s%-8s%-9d%-16s%-16s%-6d%-16s\n", type_str, action,
+           curr_rule->priority, start_ip_str, end_ip_str, curr_rule->port,
+           curr_rule->p_name);
+    curr_rule = curr_rule->next;
+  }
+  printf("\n");
+  return;
+}
+/**
+ * Insert a rule at the specified index of the chain.
+ */
+void insert_chain_rule(fw_chain *chain, int index, const uint32_t ip_start,
+                       const uint32_t ip_end, const uint8_t type,
+                       const uint16_t port, const uid_t uid,
+                       const uint8_t action, const char *p_name,
+                       const uint8_t direction) {
+  // New rule and params
+  fw_chain_rule *new_rule =
+      (struct fw_chain_rule *)malloc(sizeof(fw_chain_rule));
+  new_rule->type = type;
+  new_rule->port = port;
+  new_rule->user = uid;
+  new_rule->ip_start = ip_start;
+  new_rule->ip_end = ip_end;
+  new_rule->action = action;
+  new_rule->direction = direction;  // Package in or out]
+
+  if (p_name != NULL) {
+    strncpy(new_rule->p_name, p_name, MAX_NAME_LEN);
+  } else {
+    new_rule->p_name[0] = '\0';  // Empty string
+  }
+
+  // Find entries to modify. Add to chain entry linked list
+  add_chain_rule(chain, new_rule, index);
 }
 
 /**
  * Push a rule to the start of the chain.
  */
-void push_rule(fw_rule** head_ref, const uint32_t ip_start, const uint32_t ip_end, const uint8_t type, 
-                const uint16_t port, const uint8_t priority, const uint8_t action, const char* p_name) {
+void push_rule(fw_rule **head_ref, const uint32_t ip_start,
+               const uint32_t ip_end, const uint8_t type, const uint16_t port,
+               const uint8_t priority, const uint8_t action,
+               const char *p_name) {
+  fw_rule *new_node = (struct fw_rule *)malloc(sizeof(struct fw_rule));
 
-    fw_rule* new_node = (struct fw_rule*)malloc(sizeof(struct fw_rule));
+  new_node->type = type;
+  new_node->port = port;
+  new_node->priority = priority;
+  new_node->ip_start = ip_start;
+  new_node->ip_end = ip_end;
+  new_node->action = action;
 
-    new_node->type = type;
-    new_node->port = port;
-    new_node->priority = priority;
-    new_node->ip_start = ip_start;
-    new_node->ip_end = ip_end;
-    new_node->action = action;
+  if (p_name != NULL) {
+    strncpy(new_node->p_name, p_name, MAX_NAME_LEN);
+  } else {
+    new_node->p_name[0] = '\0';  // Empty string
+  }
 
-    if(p_name != NULL) {
-        strncpy(new_node->p_name, p_name, MAX_NAME_LEN);
-    } else {
-        new_node->p_name[0] = '\0';  // Empty string
-    }
-
-    new_node->next = (*head_ref);
-    new_node->prev = NULL;
-    if ((*head_ref) != NULL) {
-        (*head_ref)->prev = new_node;
-    }
-    (*head_ref) = new_node;
+  new_node->next = (*head_ref);
+  new_node->prev = NULL;
+  if ((*head_ref) != NULL) {
+    (*head_ref)->prev = new_node;
+  }
+  (*head_ref) = new_node;
 }
 
 /**
  * Remove the first rule that matches
  */
-void remove_rule(fw_rule** head_ref, const uint32_t ip_start, const uint32_t ip_end, const uint8_t type, 
-                    const uint16_t port, const uint8_t priority, const uint8_t action, const char* p_name) {
-
-    fw_rule *curr_rule = (*head_ref);
-    while (curr_rule != NULL){
-        if (curr_rule->ip_start == ip_start && curr_rule->ip_end == ip_end && curr_rule->action == action
-                && curr_rule->type == type && curr_rule->port == port) {
-            if(curr_rule->p_name[0] != '\0'){
-                if(p_name != NULL && strncmp(curr_rule->p_name, p_name, MAX_NAME_LEN) == 0){
-                    break;
-                }
-            } else {
-                break;
-            }
+void remove_rule(fw_rule **head_ref, const uint32_t ip_start,
+                 const uint32_t ip_end, const uint8_t type, const uint16_t port,
+                 const uint8_t priority, const uint8_t action,
+                 const char *p_name) {
+  fw_rule *curr_rule = (*head_ref);
+  while (curr_rule != NULL) {
+    if (curr_rule->ip_start == ip_start && curr_rule->ip_end == ip_end &&
+        curr_rule->action == action && curr_rule->type == type &&
+        curr_rule->port == port) {
+      if (curr_rule->p_name[0] != '\0') {
+        if (p_name != NULL &&
+            strncmp(curr_rule->p_name, p_name, MAX_NAME_LEN) == 0) {
+          break;
         }
-        curr_rule = curr_rule->next;
+      } else {
+        break;
+      }
     }
+    curr_rule = curr_rule->next;
+  }
 
-    if(curr_rule != NULL) {
-        if(curr_rule->prev != NULL){
-            curr_rule->prev->next = curr_rule->next;
-        } else {
-            // Rule was the first of the list. Need to update head.
-            (*head_ref) = curr_rule->next;
-        }
-        if(curr_rule->next != NULL){
-            curr_rule->next->prev = curr_rule->prev;
-        }
-        free(curr_rule);
+  if (curr_rule != NULL) {
+    if (curr_rule->prev != NULL) {
+      curr_rule->prev->next = curr_rule->next;
     } else {
-        printf("Did not find a matching rule to delete\n");
+      // Rule was the first of the list. Need to update head.
+      (*head_ref) = curr_rule->next;
     }
-    return;
+    if (curr_rule->next != NULL) {
+      curr_rule->next->prev = curr_rule->prev;
+    }
+    free(curr_rule);
+  } else {
+    printf("Did not find a matching rule to delete\n");
+  }
+  return;
 }
 
 #endif
